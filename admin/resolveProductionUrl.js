b4600af69310a879e95aae5c0e04c9fb2fdaedf6
frontend5 <frontend@redmelondesign.com>
2020-02-07 18:30:44 +0530

    preview page created

--- admin/resolveProductionUrl.js
+++ admin/resolveProductionUrl.js
@@ -1,3 +1,14 @@
-export default function resolveProductionUrl(document) {
-    return `https://test-1-blog.sanity.studio/preview/${document._id}`
+var previewURL="";
+
+if(location.protocol != "https") {
+    previewURL = "http://localhost:7889";
 }
+else {
+    previewURL = "https://startuphack-staging.netlify.com/";
+}
+
+
+export default function resolveProductionUrl(document) {
+    return `${previewURL}/preview-v2.html#${document._id}`
+}
+
